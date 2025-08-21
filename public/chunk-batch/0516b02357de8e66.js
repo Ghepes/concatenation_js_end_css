ec87aed79d909a20e2332d7cee0081c8e8294908
Studio Wromo Marketplace <info.wromo@gmail.com>
2025-08-21 12:38:43 +0200

    add

---
+++ public/chunk-batch/0516b02357de8e66.js
@@ -0,0 +1,9 @@
+;// __FILE_CONTENT_FOR__:0516b02357de8e66.js
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[137314],{
+
+/***/ 394335:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(905716);__web_req__(157643);__web_req__(718774);self._ebd5e19490e9b70e7436d6c497657170 = self._ebd5e19490e9b70e7436d6c497657170 || {};(function(__c) {__c.Kpa={Kxp:({V:a,Sxd:b})=>{var c,d;b=b&&b.type==="ONBOARDING_GET_STARTED"?b.bootstrap:void 0;return b===null||b===void 0||!b.Y2j||__c.Nh(642,(c=a.jl)===null||c===void 0?void 0:c.Cq)||__c.Nh(15,(d=a.jl)===null||d===void 0?void 0:d.Cq)&&a.user.Kz!=="OTHER"?!1:!0}};
+}).call(self, self._ebd5e19490e9b70e7436d6c497657170);}
+
+}])
+//# sourceMappingURL=https://chunk-composing.canva.com/chunk-batch-sm/8_sourcemaps/0516b02357de8e66.js.map
